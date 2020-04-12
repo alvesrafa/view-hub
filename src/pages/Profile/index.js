50caca7057e1e6b3f4ec3423e5b204489ad6f9e2
Rafael Alves <alvesrafael17@hotmail.com>
2020-04-11 23:57:34 -0300

    iniciando Profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image } from 'react-native';
+import api from '../../services/api';
 
 export default function({ route, navigation }){
   const { username } = route.params;
+  async function loadUser(){
+    try {
+      const response = await api.get(`/users/${username}`)
+      console.log(response.data)
+    }catch (e){
+      console.error('ERRO! ', e)
+    }
+  }
+  useEffect(() => {
+
+  }, [])
   return (
     <View>
+
       <Text>{ username }</Text>
     </View>
   )
