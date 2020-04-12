2113833ba7c8a7cdb0118aa4e118082adf2e2b3c
Rafael Alves <alvesrafael17@hotmail.com>
2020-04-11 23:48:20 -0300

    bottomNavigation inside StackNavigation

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FontAwesome } from '@expo/vector-icons';
 
@@ -7,49 +8,63 @@ import Repositories from './pages/Repositories';
 import Users from './pages/Users';
 import Profile from './pages/Profile';
 
+const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function Teste() {
+function Home() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Profile" component={Profile} />
-    </Tab.Navigator>
-  );
-}
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
 
+        if (route.name === 'Users') {
+          iconName = focused
+            ? 'user-circle'
+            : 'user-circle-o';
+        } else if (route.name === 'Repositories') {
+          iconName = focused ? 'code' : 'book';
+        }
+
+        // You can return any component that you like here!
+        return <FontAwesome name={iconName} size={size} color={color} />;
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: '#0366d6',
+      inactiveTintColor: '#000',
+    }}
+    >
+      <Tab.Screen name="Users" component={Users} />
+      <Tab.Screen name="Repositories" component={Repositories} />
+        
+    </Tab.Navigator>
+  )
+}
 export default function Routes() {
   return (
     <>
     <NavigationContainer>
-      <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Users') {
-            iconName = focused
-              ? 'user-circle'
-              : 'user-circle-o';
-          } else if (route.name === 'Repositories') {
-            iconName = focused ? 'code' : 'book';
-          }
-
-          // You can return any component that you like here!
-          return <FontAwesome name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: '#0366d6',
-        inactiveTintColor: '#000',
-      }}
-      >
-        <Tab.Screen name="Users" component={Users} />
-        <Tab.Screen name="Repositories" component={Repositories} />
-        <Tab.Screen name="Teste" component={Teste} />
-        
-      </Tab.Navigator>
-      
+      <Stack.Navigator screenOptions={{
+        headerTintColor: 'white',
+        headerStyle: { backgroundColor: 'tomato' },
+      }}>
+        <Stack.Screen 
+          name="Home" 
+          options={{
+            headerShown: false
+          }} 
+          component={Home} 
+        />
+        <Stack.Screen 
+          name="Profile" 
+          component={Profile} 
+        />
+      </Stack.Navigator>
     </NavigationContainer>
     </>
   );
 }
+
+ 
+
